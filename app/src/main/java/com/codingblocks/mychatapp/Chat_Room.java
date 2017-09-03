06f023cbfcb2052d7c9c1dd4ca95c0158f7cca60
Ravitiwari0412 <tiwaririshu19@gmail.com>
2017-09-03 16:28:16 +0530

    Initial commit

---
+++ app/src/main/java/com/codingblocks/mychatapp/Chat_Room.java
@@ -0,0 +1,111 @@
+package com.codingblocks.mychatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Chat_Room extends AppCompatActivity{
+        private Button btn_send_msg;
+        private EditText input_msg;
+        private TextView chat_conversation;
+
+        private String user_name,room_name;
+        private DatabaseReference root ;
+        private String temp_key;
+        private String msg;
+
+
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat__room);
+            btn_send_msg = (Button) findViewById(R.id.btn_send);
+            input_msg = (EditText) findViewById(R.id.etnewMsg);
+            chat_conversation = (TextView) findViewById(R.id.textView);
+
+            user_name = getIntent().getExtras().get("user_name").toString();
+            room_name = getIntent().getExtras().get("etroom").toString();
+            setTitle(" Room - "+room_name);
+
+            root = FirebaseDatabase.getInstance().getReference().getRoot().child(room_name);
+         btn_send_msg.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 DatabaseReference childroot=root.push();
+                 Map<String,Object>map=new HashMap<String,Object>();
+                 map.put("name",user_name);
+                 map.put("message",input_msg.getText().toString());
+
+                 childroot.updateChildren(map);
+//                 Map<String,Object>map=new HashMap<>();
+//                 temp_key=root.push().getKey();
+//                 root.updateChildren(map);
+//
+//                 DatabaseReference message_root=root.child(temp_key);
+//                 Map<String,Object>messagemap=new HashMap<>();
+//                 messagemap.put("name",user_name);
+//                 messagemap.put("msg",input_msg.getText().toString());
+//                 message_root.updateChildren(messagemap);
+             }
+         });
+
+            root.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s){
+                    append_chat_conversation(dataSnapshot);
+                }
+
+
+
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    append_chat_conversation(dataSnapshot);
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+        private String chat_msg,chat_user_name;
+
+        private void append_chat_conversation(DataSnapshot dataSnapshot) {
+
+         //   Iterator i = dataSnapshot.getChildren().iterator();
+
+           // while (i.hasNext()) {
+
+                chat_msg =(String) dataSnapshot.child("message").getValue();
+                chat_user_name = (String)dataSnapshot.child("name").getValue();
+
+                chat_conversation.append(chat_user_name + " : "+"\n" + chat_msg + " \n\n");
+            }
+        }
